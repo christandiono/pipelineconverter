39884a30dec15bb298ce35fe974088634ecf2e26
John Yoon <seatpost@gmail.com>
2011-10-16 00:13:18 +0000

    Test by John
    git-svn-id: https://pipelineconverter.googlecode.com/svn/trunk@8 983eb3c8-5785-a445-9050-7f41236cb99f

--- src/Converter.java
+++ src/Converter.java
@@ -2,5 +2,6 @@ public class Converter {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.out.println("Bye Bye Bye");
+		System.out.println("Bye Bye Bye");
 	}
 }
