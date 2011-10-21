a6b3fd284646d142957357b8a0be5db7a3e9a725
Sara Achour <sarachour@gmail.com>
2011-10-21 21:49:59 +0000

    Modified JSON parser to write a workflow object to a file, and parse a workflow object from a file.
    git-svn-id: https://pipelineconverter.googlecode.com/svn/trunk@27 983eb3c8-5785-a445-9050-7f41236cb99f

--- src/Parser/JSON/JSONParser.java
+++ src/Parser/JSON/JSONParser.java
@@ -1,7 +1,9 @@
 package Parser.JSON;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
@@ -13,12 +15,40 @@ import Parser.Parser;
 
 public class JSONParser implements Parser {
 	
-		public static void parse() throws JsonSyntaxException, JsonIOException, FileNotFoundException{
+		public static void parse(String filepath) {
 			
 			Gson gson = GalaxyJSONLoader.Loader;
 			//Parse workflow
-			Workflow egg = gson.fromJson(new FileReader("src/Parser/test.ga"),
-									Workflow.class);
-			System.out.println(gson.toJson(egg));
+			Workflow egg = null;
+			try {
+				egg = gson.fromJson(new FileReader(filepath),
+										Workflow.class);
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonIOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		public static void generate(String path, Workflow w){
+			Gson gson = GalaxyJSONLoader.Loader;
+			String jsonText = gson.toJson(w);
+			try{
+				  // Create file 
+				  FileWriter fstream = new FileWriter(path);
+				  BufferedWriter out = new BufferedWriter(fstream);
+				  out.write(jsonText);
+				  //Close the output stream
+				  out.close();
+				  }catch (Exception e){//Catch exception if any
+				  System.err.println("Error: " + e.getMessage());
+				  }
+			
 		}
 }
