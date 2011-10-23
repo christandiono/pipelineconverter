2160eab832b7e57c65a6aec4c24ba80cb63b2a49
Chris Tandiono <chris.tandiono@gmail.com>
2011-10-23 09:08:40 +0000

    Minor code cleanup for Printer class. Add a method for the caller to specify their own OutputStream, for example for outputting .xml files.
    git-svn-id: https://pipelineconverter.googlecode.com/svn/branches/chris@68 983eb3c8-5785-a445-9050-7f41236cb99f

--- src/CLInterface/Printer.java
+++ src/CLInterface/Printer.java
@@ -12,13 +12,22 @@ import java.io.OutputStream;
  */
 public class Printer {
 	
-	private static void actuallyPrint(String[] messages, OutputStream destination) {
+	/**
+	 * Prints messages to destination.
+	 * @param destination OutputStream to write to. If null, doesn't actually write anything to anywhere.
+	 * @param messages Strings to write. Doesn't write null strings at all (as opposed to outputting a blank line, for example).
+	 */
+	private static void actuallyPrint(OutputStream destination, String[] messages) {
+		if (destination == null) { /* no point actually doing anything */
+			return;
+		}
 		String message;
 		for (int i = 0; i < messages.length; i++) {
 			message = messages[i];
-			if (destination != null && message != null) {
+			if (message != null) { /* null: ignore */
 				try {
 					destination.write(message.getBytes());
+					destination.write("\n".getBytes());
 					destination.flush();
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -27,12 +36,32 @@ public class Printer {
 		}
 	}
 	
+	/**
+	 * Print to System.err, if so defined on the command-line.
+	 * 
+	 * @param messages Message(s) to write.
+	 */
 	public static void log(String... messages) {
-		actuallyPrint(messages, ConverterConfig.DEBUG);
+		actuallyPrint(ConverterConfig.DEBUG, messages);
 	}
 	
+	/**
+	 * Print to output as configured on the command-line.
+	 * 
+	 * @param messages Message(s) to write.
+	 */
 	public static void output(String... messages) {
-		actuallyPrint(messages, ConverterConfig.OUTPUT);
+		actuallyPrint(ConverterConfig.OUTPUT, messages);
+	}
+	
+	/**
+	 * Print with custom OutputStream destination.
+	 * 
+	 * @param destination OutputStream to write to.
+	 * @param messages Message(s) to write.
+	 */
+	public static void output(OutputStream destination, String... messages) {
+		actuallyPrint(destination, messages);
 	}
 	
 }
