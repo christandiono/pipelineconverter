a74272e593053a301f1a030dc7143d18f4ac4864
Sara Achour <sarachour@gmail.com>
2011-10-26 00:25:11 +0000

    git-svn-id: https://pipelineconverter.googlecode.com/svn/trunk@116 983eb3c8-5785-a445-9050-7f41236cb99f

--- src/FileOps/Generator.java
+++
@@ -1,8 +0,0 @@
-package FileOps;
-
-import java.io.IOException;
-
-public interface Generator<T> {
-	public void generate(T object, String path) throws IOException;
-	public String generate(T object);
-}
