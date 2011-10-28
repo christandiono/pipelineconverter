8d34459701d9d7e5511583f4a938137643a76635
Johnny Ho <johnny5550822@gmail.com>
2011-10-28 23:16:58 +0000

    git-svn-id: https://pipelineconverter.googlecode.com/svn/trunk@144 983eb3c8-5785-a445-9050-7f41236cb99f

--- src/LONI/tree/Pipeline.java
+++ src/LONI/tree/Pipeline.java
@@ -1,14 +1,6 @@
 package LONI.tree;
 
-public class Pipeline extends LoniNode{
-	Float Version;
-	ModuleGroup Workflow;
+public class Pipeline extends LoniNode {
 	
-	public Pipeline(){
-		Workflow = new ModuleGroup();
-	}
-
-	public ModuleGroup getRootModuleGroup(){
-		return Workflow;
-	}
+	String Version
 }
